716a80202e334878be6a85966c10a28cbcc2d71f
David Gipe <dwgipe@gmail.com>
2017-01-22 19:18:27 -0500

    Create classes and begin work on csv file load

--- assignment1/Program.cs
+++ assignment1/Program.cs
@@ -10,6 +10,33 @@ namespace assignment1
     {
         static void Main(string[] args)
         {
+            // Create class instances
+            UserInterface ui = new UserInterface();
+            CSVProcessor csv = new CSVProcessor();
+            WineItemCollection wic = new WineItemCollection();
+            WineItem wines = new WineItem();
+
+            //Recieve user input
+            int choice = ui.GetUserInput();
+
+            //While the chouse they slected is not 2, continue to do work
+            while (choice != 2)
+            {
+                //See if the input they sent is equal to 1.
+                if (choice == 1)
+                {
+                    //Load CSV file
+                    csv.LoadWine();
+
+
+
+
+                }
+
+                //re-prompt te user for input
+                choice = ui.GetUserInput();
+            }
+
         }
     }
 }
---
+++ assignment1/UserInterface.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment1
+{
+    class UserInterface
+    {
+        //*****************************
+        //Methods
+        //*****************************
+        public int GetUserInput()
+        {
+            //Request load
+            this.LoadMenu();  //calling a method
+
+            //Get the input from the user
+            String input = Console.ReadLine();
+
+            //While te imput is not valid re-get the input
+            while (input != "1" && input != "2")
+            {
+
+                //Print Error message
+                this.PrintErrorMessage();
+
+                //re-spring the menu
+                this.LoadMenu();
+
+                //et the input from the console again
+                input = Console.ReadLine();
+
+            }
+            //At this pint, I kow the input is a valid number of either
+            //a 1 or a 2.
+
+            return Int32.Parse(input);
+        }
+
+        public void Output(string outputString)
+        {
+            Console.WriteLine(outputString);
+        }
+
+
+        private void LoadMenu()
+        {
+            Console.WriteLine("Load Wine list?");
+            Console.WriteLine("1. Yes");
+            Console.WriteLine("2. No. Exit");
+        }
+
+        private void PrintErrorMessage()
+        {
+            Console.WriteLine("That is not a valid entry Dave");
+            Console.WriteLine("Please make a valid choice");
+            Console.WriteLine();
+        }
+    }
+
+
+}
